1eb7a8f9daf02abbfa4623dea12b4b29f07439c2
Yash Babariya <67451545+yashbabariya@users.noreply.github.com>
2024-07-09 13:04:10 +0000

    changes in main.tf file

--- Task 2/main.tf
+++ Task 2/main.tf
@@ -13,19 +13,19 @@ provider "google" {
 
 module "vpc" {
   source                    = "./modules/vpc"
-  region                    = "asia-south1"
-  project_id                = "task-terraform-428814"
-  vpc_name                  = "demo"
-  public_subnet_cidr_range  = "10.0.1.0/24"
-  private_subnet_cidr_range = "10.0.2.0/24"
+  region                    = "<region-name>"
+  project_id                = "<project-id>"
+  vpc_name                  = "<vpc-name>"
+  public_subnet_cidr_range  = "<cidr-range>"
+  private_subnet_cidr_range = "<cidr-range>"
 }
 
 module "cloudrun" {
   source                    = "./modules/cloudrun"
-  region                    = "asia-south1"
-  project_id                = "task-terraform-428814"
-  docker_image              = "asia-south1-docker.pkg.dev/task-terraform-428814/simple-app/myreactapp:v1"
-  cloudrun_name             = "reactapp-demo"
-  cloudrun_resources_cpu    = "0.5"
-  cloudrun_resources_memory = "200Mi"
+  region                    = "<region-name>"
+  project_id                = "<project-id>"
+  docker_image              = "<image-name>"
+  cloudrun_name             = "<cloudrun-name>"
+  cloudrun_resources_cpu    = "<cpu>"
+  cloudrun_resources_memory = "<memory>"
 }
